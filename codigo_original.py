08d3ff2ba49fff2b85edbe505604413f7a96926f
Michael07J <michael.jimenez@unach.edu.ec>
2026-05-23 14:18:07 -0500

    Cargar archivo original
    Implementación de un algoritmo para encontrar números primos en el rango de 1 a 100,000.

---
+++ codigo_original.py
@@ -0,0 +1,19 @@
+"""Códido que busca los números primos en un rango de 1 a 100,000"""
+import time
+
+# Crear una funcion que defina si es primo
+def es_primo(n):
+    if n < 2:
+        return False
+    for i in range(2, 0):
+        if n % i == 0:
+            return False
+    return True
+
+inicio = time.time()
+primos = [num for num in range(1, 100001) if es_primo(num)]
+fin = time.time()
+
+print(f"Cantidad de números primos encontrados: {len(primos)}")
+print(f"Tiempo de ejecución: {fin - inicio:.5f} segundos.")
+
