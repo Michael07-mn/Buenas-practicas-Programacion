2ace6ee9c04c6ba9133a0596cc65add9e414e3e0
Michael07J <michael.jimenez@unach.edu.ec>
2026-05-23 16:21:39 -0500

    Fix prime number calculation logic in script

--- codigo_original.py
+++ codigo_original.py
@@ -5,7 +5,7 @@ import time
 def es_primo(n):
     if n < 2:
         return False
-    for i in range(2, 0):
+    for i in range(2, n):
         if n % i == 0:
             return False
     return True
